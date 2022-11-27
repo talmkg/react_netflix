db6762276e29067844d51a376e1cec3113090dc0
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-11-27 14:24:40 +0100

    final touches.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,31 +1,48 @@
 import { Component } from "react";
-import { Carousel, Stack } from "react-bootstrap";
+import { Carousel, Stack, Spinner } from "react-bootstrap";
 class carousel extends Component {
   state = {
     key1: [],
     key2: [],
     key3: [],
+    isLoading: true,
   };
 
   movieSearch = async () => {
-    const response1 = await fetch(
-      "http://www.omdbapi.com/?apikey=e13e47fa&s=harry%20potter"
-    );
-    const response2 = await fetch(
-      "http://www.omdbapi.com/?apikey=e13e47fa&s=lord%20of%20the%20rings"
-    );
-    const response3 = await fetch(
-      "http://www.omdbapi.com/?apikey=e13e47fa&s=star%20wars"
-    );
-    const unboxedResponse1 = await response1.json();
-    const unboxedResponse2 = await response2.json();
-    const unboxedResponse3 = await response3.json();
-    this.setState({
-      key1: unboxedResponse1,
-      key2: unboxedResponse2,
-      key3: unboxedResponse3,
-    });
-    console.log(unboxedResponse2);
+    try {
+      const response1 = await fetch(
+        "http://www.omdbapi.com/?apikey=e13e47fa&s=harry%20potter"
+      );
+      const response2 = await fetch(
+        "http://www.omdbapi.com/?apikey=e13e47fa&s=lord%20of%20the%20rings"
+      );
+      const response3 = await fetch(
+        "http://www.omdbapi.com/?apikey=e13e47fa&s=star%20wars"
+      );
+      if (response1.ok && response2.ok && response3.ok) {
+        const unboxedResponse1 = await response1.json();
+        const unboxedResponse2 = await response2.json();
+        const unboxedResponse3 = await response3.json();
+        this.setState({
+          key1: unboxedResponse1,
+          key2: unboxedResponse2,
+          key3: unboxedResponse3,
+          isLoading: false,
+        });
+      } else {
+        console.log("Error! Danger! Run!");
+        this.setState({
+          ...this.state,
+          isLoading: false,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        ...this.state,
+        isLoading: false,
+      });
+    }
   };
 
   componentDidMount() {
@@ -35,6 +52,15 @@ class carousel extends Component {
   render() {
     return (
       <>
+        {this.state.isLoading && (
+          <Spinner
+            animation="border"
+            role="status"
+            className="custom-spinner-color"
+          >
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        )}
         <h4>Trending Now</h4>
 
         <Carousel fade>
